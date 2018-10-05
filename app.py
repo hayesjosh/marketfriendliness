790b20363ad29368d119e20ddd813ca37dd28e12
Joshua David Hayes <hayesjosh@gmail.com>
2018-10-04 21:31:12 -0700

    updates to all

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #importing (taken from my first flask project)
 from flask import Flask, render_template, request, redirect
-import feather
+# import feather
+import pickle
 # import requests
 # import pandas as pd
 from bokeh.plotting import figure, output_file, show
@@ -11,7 +12,8 @@ import os
 # #importing (taken from bokeh county-map template)
 from bokeh.io import show
 from bokeh.models import LogColorMapper
-from bokeh.palettes import Viridis6 as palette
+# from bokeh.palettes import Viridis6 as palette
+from bokeh.palettes import RdBu10 as palette
 from bokeh.plotting import figure
 # from bokeh.sampledata.us_counties import data as counties
 # from bokeh.sampledata.unemployment import data as unemployment
@@ -32,59 +34,75 @@ import xml.etree.cElementTree as et
 #     else:
 #         return open(filename, 'r', newline='', encoding='utf8')
 
+#
+# #code to read in the counties data
+# def _read_data():
+#     '''
+#     '''
+#     nan = float('NaN')
+#
+#     data = {}
+#
+#     with open('data/US_Counties.csv') as f:
+#         next(f)
+#         reader = csv.reader(f, delimiter=str(','), quotechar=str('"'))
+#         for row in reader:
+#             name, dummy, state, dummy, geometry, dummy, dummy, dummy, det_name, state_id, county_id, dummy, dummy = row
+#             xml = et.fromstring(geometry)
+#             lats = []
+#             lons = []
+#             for i, poly in enumerate(xml.findall('.//outerBoundaryIs/LinearRing/coordinates')):
+#                 if i > 0:
+#                     lats.append(nan)
+#                     lons.append(nan)
+#                 coords = (c.split(',')[:2] for c in poly.text.split())
+#                 lat, lon = list(zip(*[(float(lat), float(lon)) for lon, lat in
+#                     coords]))
+#                 lats.extend(lat)
+#                 lons.extend(lon)
+#             data[(int(state_id), int(county_id))] = {
+#                 'name' : name,
+#                 'detailed name' : det_name,
+#                 'state' : state,
+#                 'lats' : lats,
+#                 'lons' : lons,
+#             }
+#
+#     return data
+#
+# def _read_unemploy():
+#     '''
+#     '''
+#     data = {}
+#     with open('data/unemployment09.csv') as f:
+#         reader = csv.reader(f, delimiter=str(','), quotechar=str('"'))
+#         for row in reader:
+#             dummy, state_id, county_id, dumm, dummy, dummy, dummy, dummy, rate = row
+#             data[(int(state_id), int(county_id))] = float(rate)
+#     return data
+#
+# unemployment = _read_unemploy()
 
-#code to read in the counties data
-def _read_data():
-    '''
-    '''
-    nan = float('NaN')
-
-    data = {}
-
-    with open('data/US_Counties.csv') as f:
-        next(f)
-        reader = csv.reader(f, delimiter=str(','), quotechar=str('"'))
-        for row in reader:
-            name, dummy, state, dummy, geometry, dummy, dummy, dummy, det_name, state_id, county_id, dummy, dummy = row
-            xml = et.fromstring(geometry)
-            lats = []
-            lons = []
-            for i, poly in enumerate(xml.findall('.//outerBoundaryIs/LinearRing/coordinates')):
-                if i > 0:
-                    lats.append(nan)
-                    lons.append(nan)
-                coords = (c.split(',')[:2] for c in poly.text.split())
-                lat, lon = list(zip(*[(float(lat), float(lon)) for lon, lat in
-                    coords]))
-                lats.extend(lat)
-                lons.extend(lon)
-            data[(int(state_id), int(county_id))] = {
-                'name' : name,
-                'detailed name' : det_name,
-                'state' : state,
-                'lats' : lats,
-                'lons' : lons,
-            }
-
-    return data
-
-def _read_unemploy():
-    '''
-    '''
-    data = {}
-    with open('data/unemployment09.csv') as f:
-        reader = csv.reader(f, delimiter=str(','), quotechar=str('"'))
-        for row in reader:
-            dummy, state_id, county_id, dumm, dummy, dummy, dummy, dummy, rate = row
-            data[(int(state_id), int(county_id))] = float(rate)
-    return data
-
-unemployment = _read_unemploy()
-
+palette.reverse()
+with open('data/county_names.p', 'rb') as fp:
+    county_names = pickle.load(fp)
+with open('data/county_xs.p', 'rb') as fp:
+    county_xs = pickle.load(fp)
+with open('data/county_ys.p', 'rb') as fp:
+    county_ys = pickle.load(fp)
+with open('data/county_fico.p', 'rb') as fp:
+    county_fico = pickle.load(fp)
+with open('data/county_fthb.p', 'rb') as fp:
+    county_fthb = pickle.load(fp)
 
 
 #starting up our app
 app = Flask(__name__)
+
+
+
+
+
 
 #the primary page
 @app.route('/')
@@ -106,26 +124,36 @@ def index():
 @app.route('/index',methods=['GET','POST'])
 def index_tick():
     ####Request was POST, let's use that info to make a custom graph!
+    #
+    # counties = _read_data()
+    # counties = {
+    #     code: county for code, county in counties.items() if county["state"] == "tx"
+    # }
+    #
+    # county_xs = [county["lons"] for county in counties.values()]
+    # county_ys = [county["lats"] for county in counties.values()]
+    #
+    # county_names = [county['name'] for county in counties.values()]
+    # county_rates = [unemployment[county_id] for county_id in counties]
+    #importing data
 
-    counties = _read_data()
-    counties = {
-        code: county for code, county in counties.items() if county["state"] == "tx"
-    }
 
-    county_xs = [county["lons"] for county in counties.values()]
-    county_ys = [county["lats"] for county in counties.values()]
-
-    county_names = [county['name'] for county in counties.values()]
-    county_rates = [unemployment[county_id] for county_id in counties]
     color_mapper = LogColorMapper(palette=palette)
+    # color_mapper = LogColorMapper(palette='RdBu')
+
+    # user_fico = request.form['credit_input']
+    user_diff = [720 - x for x in county_fico]
+
 
     data=dict(
         x=county_xs,
         y=county_ys,
         name=county_names,
-        rate=county_rates,
+        fico=county_fico,
+        fthb=county_fthb,
+        credit_diff=user_diff
     )
-    palette.reverse()
+
 
     TOOLS = "pan,wheel_zoom,reset,hover,save"
 
@@ -134,19 +162,20 @@ def index_tick():
         x_axis_location=None, y_axis_location=None,
         tooltips=[
             ("County Name", "@name"),
-            # ("Avg. Credit Score", "($x, $y)"),
-            # ("Avg. Income", "@rate%"),
-            # ("Avg. Home Value", "@rate%"),
-            ("Your competitiveness in 2019", "@rate%")
+            ("Avg. Credit Score", "@fico"),
+            ("Market Competitiveness", "@fthb"),
+            ("Your Credit Surplus", "@credit_diff")
+            # ("Credit Diff", "@test1")
+            # ("Your competitiveness in 2019", "@ficodiff")
         ])
     p.grid.grid_line_color = None
     p.hover.point_policy = "follow_mouse"
 
     p.patches('x', 'y', source=data,
-              fill_color={'field': 'rate', 'transform': color_mapper},
+              fill_color={'field': 'fthb', 'transform': color_mapper},
               fill_alpha=0.7, line_color="white", line_width=0.5)
 
-    # user_fico = request.form['credit_input']
+
 
     script, div = components(p)
     #sending user over to the newly made graph.html
