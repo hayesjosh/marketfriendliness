f8b15fdbfc1cab4de885ec4b3e71bd5f77a60666
Joshua D Hayes <hayesjosh@gmail.com>
2018-11-06 14:16:07 -0800

    debug off

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ if __name__ == '__main__':
   #port = int(os.environ.get("PORT", 5000))
   #app.run(host='0.0.0.0', port=port)
 #  app.run(port=33507)
-  # app.run(port=5000)
-  app.run(port=5000, debug=True)
+  app.run(port=5000)
+  # app.run(port=5000, debug=True)
